69af64c93b4d5b44162b7df0ac2ca6d750470a45
github username <github email address>
2019-08-06 10:29:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+ 
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+ 
+//   return gifts;
+// }
+ 
+// wrapGifts(gifts);
+
+const friends = ["Lisa", "Kaitlin", "Jan"]
+const event = ["surprise"]
+let messages = []
+
+function writeCards(friends, event) {
+    for (let i = 0; i < friends.length; i++) {
+        messages.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+
+
+// let countup = 0;
+// while (countup < 10) {
+//   console.log(countup++);
+// }
+function countdown(n) {
+    let countdown = n;
+        while (countdown >= 0) {
+            console.log(countdown--);
+        }
+}  
+countdown(10); 
